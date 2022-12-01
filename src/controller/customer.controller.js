62946377fc219aa8f2b5494438ffe3f598b62a47
Jose Augusto de Oliveira Pins <joliveirapins.jaop@gmail.com>
2022-12-01 20:11:32 -0300

    corrigindo validação

--- src/controller/customer.controller.js
+++ src/controller/customer.controller.js
@@ -2,10 +2,10 @@ const service = require("../service/customer.service");
 
 const create = async (req, res) => {
   await service.create(req.body);
-  if (res) {
+  try {
     res.status(201).send("Customer created successfully");
     
-  } else {
+  } catch (error) {
     res.status(400).send("Something is wrong in Customer")
   }
   
@@ -19,10 +19,10 @@ const getAll = async (req, res) => {
 const remove = async (req, res) => {
   const provaId = req.params.id;
   await service.remove(provaId);
-  if (res) {
+  try {
     res.status(204).send("Customer removed successfully");
 
-  } else {
+  } catch (error) {
     res.status(404).send("Something is wrong in Customer")
   }
   
--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -2,13 +2,12 @@ const service = require("../service/product.service");
 
 const create = async (req, res) => {
   await service.create(req.body);
-  if (res) {
+  try {
     res.status(201).send("Product created successfully");
 
-  } else {
-    res.status(400).send("Something is wrong in Product")
+  } catch (erro) {
+    res.status(400).send("Something is wrong in Product");
   }
- 
 };
 
 const getAll = async (req, res) => {
@@ -19,13 +18,12 @@ const getAll = async (req, res) => {
 const remove = async (req, res) => {
   const provaId = req.params.id;
   await service.remove(provaId);
-  if (res) {
+  try {
     res.status(204).send("Product removed successfully");
-
-  } else {
-    res.status(404).send("Something is wrong in Product")
+    
+  } catch (error) {
+    res.status(404).send("Something is wrong in Product");
   }
-  
 };
 
 module.exports = {
